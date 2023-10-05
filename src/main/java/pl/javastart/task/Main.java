c7dbcda2e2422e0d1f4e810bf67a4ffb29719739
Maciej Mazur <maciej.mazur.mailbox@gmail.com>
2023-10-05 16:36:18 +0200

    solution

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,7 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-
+        ArrayCalculator arrayCalculator = new ArrayCalculator();
+        arrayCalculator.run();
     }
 }
